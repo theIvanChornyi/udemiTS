c5693e0dc6e81f4f20bf5c756e2d0b3432787a0b
Ivan Chornyi <107806721+theIvanChornyi@users.noreply.github.com>
2023-02-25 13:08:38 +0200

    complited 16

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-var currRate = '1.05';
-var fetchCurr = function (response) {
-    var data = JSON.parse(response);
-    return data;
+var electricityUserData = {
+    readings: 95,
+    units: 'kWt',
+    mode: 'double'
 };
-function transferEurToUsd(available, amount, commission) {
-    if (available) {
-        var res = fetchCurr(currRate) * amount * commission;
-        console.log(res);
-        // Или запись в элемент на странице вместо консоли
+var waterUserData = {
+    readings: 3,
+    units: 'm3'
+};
+var elRate = 0.45;
+var wRate = 2;
+var monthPayments = [0, 0]; // [electricity, water]
+var calculatePayments = function (_a, _b, elRate, wRate) {
+    var mode = _a.mode, readings = _a.readings;
+    var wData = _b.readings;
+    if (mode === 'double' && readings < 50) {
+        monthPayments[0] = readings * elRate * 0.7;
     }
     else {
-        console.log('Сейчас обмен недоступен');
+        monthPayments[0] = readings * elRate;
     }
-}
-transferEurToUsd(true, 500, 1.05);
+    monthPayments[1] = wData * wRate;
+};
+calculatePayments(electricityUserData, waterUserData, elRate, wRate);
+var sendInvoice = function (_a, _b, _c) {
+    var electricity = _a[0], water = _a[1];
+    var eReadings = _b.readings, eUnits = _b.units;
+    var wReadings = _c.readings, wUnits = _c.units;
+    var text = "    Hello!\n    This month you used ".concat(eReadings, " ").concat(eUnits, " of electricity\n    It will cost: ").concat(electricity, "\u20AC\n    \n    This month you used ").concat(wReadings, " ").concat(wUnits, " of water\n    It will cost: ").concat(water, "\u20AC");
+    return text;
+};
+console.log(sendInvoice(monthPayments, electricityUserData, waterUserData));
--- index.ts
+++ index.ts
@@ -1,22 +1,49 @@
-const currRate: string = '1.05';
-
-const fetchCurr = (response: string): number => {
-  const data: number = JSON.parse(response);
-  return data;
+const electricityUserData = {
+  readings: 95,
+  units: 'kWt',
+  mode: 'double',
 };
 
-function transferEurToUsd(
-  available: boolean,
-  amount: number,
-  commission: number
-): void {
-  if (available) {
-    let res: number = fetchCurr(currRate) * amount * commission;
-    console.log(res);
-    // Или запись в элемент на странице вместо консоли
-  } else {
-    console.log('Сейчас обмен недоступен');
-  }
-}
+const waterUserData = {
+  readings: 3,
+  units: 'm3',
+};
 
-transferEurToUsd(true, 500, 1.05);
+const elRate: number = 0.45;
+const wRate: number = 2;
+
+const monthPayments: number[] = [0, 0]; // [electricity, water]
+
+const calculatePayments = (
+  { mode, readings }: { mode: string; readings: number },
+  { readings: wData }: { readings: number },
+  elRate: number,
+  wRate: number
+): void => {
+  if (mode === 'double' && readings < 50) {
+    monthPayments[0] = readings * elRate * 0.7;
+  } else {
+    monthPayments[0] = readings * elRate;
+  }
+
+  monthPayments[1] = wData * wRate;
+};
+
+calculatePayments(electricityUserData, waterUserData, elRate, wRate);
+
+const sendInvoice = (
+  [electricity, water]: number[],
+  { readings: eReadings, units: eUnits }: { readings: number; units: string },
+  { readings: wReadings, units: wUnits }: { readings: number; units: string }
+): string => {
+  const text: string = `    Hello!
+    This month you used ${eReadings} ${eUnits} of electricity
+    It will cost: ${electricity}€
+    
+    This month you used ${wReadings} ${wUnits} of water
+    It will cost: ${water}€`;
+
+  return text;
+};
+
+console.log(sendInvoice(monthPayments, electricityUserData, waterUserData));
