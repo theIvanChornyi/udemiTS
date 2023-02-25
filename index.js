9b4abcd6045a663ee1f8c93337f5b3fdaf6e4814
Ivan Chornyi <107806721+theIvanChornyi@users.noreply.github.com>
2023-02-25 12:51:29 +0200

    complited 10

---
+++ index.js
@@ -0,0 +1,16 @@
+var currRate = '1.05';
+var fetchCurr = function (response) {
+    var data = JSON.parse(response);
+    return data;
+};
+function transferEurToUsd(available, amount, commission) {
+    if (available) {
+        var res = fetchCurr(currRate) * amount * commission;
+        console.log(res);
+        // Или запись в элемент на странице вместо консоли
+    }
+    else {
+        console.log('Сейчас обмен недоступен');
+    }
+}
+transferEurToUsd(true, 500, 1.05);
